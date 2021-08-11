90ee2ff7faf38a36f200c10cafe45b8ac9cc48d4
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2021-08-11 09:59:21 -0400

    Initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f0a574bfbc0a5a6edb691a3445004784'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3d2dc53b-7cad-4709-af80-d6d9899cbd5d'
+};
